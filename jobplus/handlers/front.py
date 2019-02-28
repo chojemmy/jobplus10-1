d6d5c4732b775a63e08dc833209665f33fd0dfa3
chenanming <13133832749@163.com>
2019-02-28 14:19:49 +0800

    logout

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, redirect, url_for
-from flask_login import login_user
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
 from jobplus.models import User
 from jobplus.forms import LoginForm
 
@@ -18,3 +18,9 @@ def login():
         return redirect(url_for('.index'))
     return render_template('login.html', form=form)
 
+@front.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You are logged in', 'success')
+    return redirect(url_for('.index'))
